e25c9030e944a0247149469835bd1b71c019d768
Daniel Dunbar <dunbardanielj@gmail.com>
2022-11-08 16:29:02 -0500

    Add placeholders elements for chat

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { init, animate } from './scene';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <canvas id="bg"></canvas>
+    <main>
+      <div class="chat-container">
+        <div class="chat-history">History</div>
+        <div class="chat-input">Input here</div>
+      </div>
+    </main>
   </div>
 `;
 
